6fdc4552be00713107837b7da071ac18000babb5
Damjan <dpavlica@itekako.com>
2016-11-07 19:16:03 +0100

    remove coment

--- src/scene.c
+++ src/scene.c
@@ -1,46 +1,7 @@
-
-/* Copyright (c) Mark J. Kilgard, 1994. */
-
-/**
- * (c) Copyright 1993, Silicon Graphics, Inc.
- * ALL RIGHTS RESERVED 
- * Permission to use, copy, modify, and distribute this software for 
- * any purpose and without fee is hereby granted, provided that the above
- * copyright notice appear in all copies and that both the copyright notice
- * and this permission notice appear in supporting documentation, and that 
- * the name of Silicon Graphics, Inc. not be used in advertising
- * or publicity pertaining to distribution of the software without specific,
- * written prior permission. 
- *
- * THE MATERIAL EMBODIED ON THIS SOFTWARE IS PROVIDED TO YOU "AS-IS"
- * AND WITHOUT WARRANTY OF ANY KIND, EXPRESS, IMPLIED OR OTHERWISE,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF MERCHANTABILITY OR
- * FITNESS FOR A PARTICULAR PURPOSE.  IN NO EVENT SHALL SILICON
- * GRAPHICS, INC.  BE LIABLE TO YOU OR ANYONE ELSE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, INDIRECT OR CONSEQUENTIAL DAMAGES OF ANY
- * KIND, OR ANY DAMAGES WHATSOEVER, INCLUDING WITHOUT LIMITATION,
- * LOSS OF PROFIT, LOSS OF USE, SAVINGS OR REVENUE, OR THE CLAIMS OF
- * THIRD PARTIES, WHETHER OR NOT SILICON GRAPHICS, INC.  HAS BEEN
- * ADVISED OF THE POSSIBILITY OF SUCH LOSS, HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, ARISING OUT OF OR IN CONNECTION WITH THE
- * POSSESSION, USE OR PERFORMANCE OF THIS SOFTWARE.
- * 
- * US Government Users Restricted Rights 
- * Use, duplication, or disclosure by the Government is subject to
- * restrictions set forth in FAR 52.227.19(c)(2) or subparagraph
- * (c)(1)(ii) of the Rights in Technical Data and Computer Software
- * clause at DFARS 252.227-7013 and/or in similar or successor
- * clauses in the FAR or the DOD or NASA FAR Supplement.
- * Unpublished-- rights reserved under the copyright laws of the
- * United States.  Contractor/manufacturer is Silicon Graphics,
- * Inc., 2011 N.  Shoreline Blvd., Mountain View, CA 94039-7311.
- *
- * OpenGL(TM) is a trademark of Silicon Graphics, Inc.
- */
 /*
  *  scene.c
  *  This program demonstrates the use of the GL lighting model.
- *  Objects are drawn using a grey material characteristic. 
+ *  Objects are drawn using a grey material characteristic.
  *  A single light source illuminates the objects.
  */
 #include <stdlib.h>
@@ -48,7 +9,7 @@
 
 /*  Initialize material property and light source.
  */
-void 
+void
 myinit(void)
 {
     GLfloat light_ambient[] =
@@ -72,7 +33,7 @@ myinit(void)
     glEnable(GL_LIGHTING);
 }
 
-void 
+void
 display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -102,7 +63,7 @@ display(void)
     glFlush();
 }
 
-void 
+void
 myReshape(int w, int h)
 {
     glViewport(0, 0, w, h);
@@ -117,7 +78,7 @@ myReshape(int w, int h)
     glMatrixMode(GL_MODELVIEW);
 }
 
-void 
+void
 polygon_mode(int value)
 {
     switch (value) {
@@ -140,7 +101,7 @@ polygon_mode(int value)
     glutPostRedisplay();
 }
 
-void 
+void
 main_menu(int value)
 {
     if (value == 666)
@@ -148,10 +109,10 @@ main_menu(int value)
 }
 
 /*  Main Loop
- *  Open window with initial window size, title bar, 
+ *  Open window with initial window size, title bar,
  *  RGBA display mode, and handle input events.
  */
-int 
+int
 main(int argc, char **argv)
 {
     int submenu;
