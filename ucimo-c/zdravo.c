a637d47f7b69baf26758e5990cc7e4a0130127dd
Damjan <dpavlica@itekako.com>
2016-11-08 12:55:22 +0100

    define

--- ucimo-c/zdravo.c
+++ ucimo-c/zdravo.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+#define PI 3.1415
 char poruka[] = "Zdravo Svete!";
 int min = 3;
 int max = 4;
@@ -10,6 +11,8 @@ int saberi(int x, int y) {
 
 int main() {
   printf("%s\n", poruka);
+
+  printf("PI je %f\n", PI);
 
   printf("Zbir je %d\n", saberi(min, max));
 
